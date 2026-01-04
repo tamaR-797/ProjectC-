70e50509ac1f8ec44cb279a5d10df7cb6f9c7de0
הנדסת תוכנה <huser@sem.local>
2026-01-04 13:54:36 +0200

    adding one char

--- DalTest/Initalization.cs
+++ DalTest/Initalization.cs
@@ -6,7 +6,7 @@ using Dal;
 using DO;
 using DalApi;
 
-namespace DalTest;
+namespace DalTest {
 
 public static class Initalization
 {
@@ -17,17 +17,17 @@ public static class Initalization
     private static void createSale()
     {
         SaleImplementation si = new SaleImplementation(s_dal);
-        si.create(new Sale { EndDate = DateTime.Now,IsClub=true,ProdId=5,QuantitySale=33,SaleId=1,SalePrice=100,StartDate=null });
+        si.create(new Sale { EndDate = DateTime.Now, IsClub = true, ProdId = 5, QuantitySale = 33, SaleId = 1, SalePrice = 100, StartDate = null });
     }
     private static void createCustomer()
     {
-        CustomerImplementation ci =new CustomerImplementation(s_dal);
-        ci.create(new Customer { CustAddress = "gitf", CustId = 1, CustName = "ggf", CustPhone = "05333" });
+        CustomerImplementation ci = new CustomerImplementation(s_dal);
+        ci.create(new Customer { CustAddress = "Netivot Shalom 7", CustId = 1, CustName = "Naama Veig", CustPhone = "0533110130" });
     }
     private static void createProduct()
     {
         ProductImplementation pi = new ProductImplementation(s_dal);
-        pi.create(new Product {category=Categories.DRESSES,ProdId=5,ProdName="gfg",ProdPrice=100,QuantityInStock=100 })
+        pi.create(new Product { category = Categories.DRESSES, ProdId = 5, ProdName = "gfg", ProdPrice = 100, QuantityInStock = 100 });
     }
     public static void initialize(IDal dal)
     {
@@ -36,4 +36,5 @@ public static class Initalization
         createCustomer(s_dal.Customer);
         createProduct(s_dal.Product);
     }
+}
 }
